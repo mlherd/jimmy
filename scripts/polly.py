4735ff843186bd6797894099fc3af6cf4628eedf
Melih Erdogan <h.meliherdogan@gmail.com>
2018-02-19 17:53:59 -0800

    Update polly.py

--- scripts/polly.py
+++ scripts/polly.py
@@ -3,5 +3,4 @@ import subprocess
 
 command = 'aws polly synthesize-speech --text-type ssml --text "<speak>Hey Gabi! I am so ready to be programmed to do great things! and attacking is not one of them</speak>" --output-format mp3 --voice-id Justin /home/melih/Desktop/speech.mp3'
 
-file_name = 'polly_out.mp3'
 subprocess.call(command, shell=True)
